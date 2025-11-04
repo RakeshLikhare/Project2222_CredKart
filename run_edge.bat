72c675ddffd7a5c790695b9689e97b287e265853
RAKESH LIKHARE <rakeshlikhare@gmail.com>
2025-11-04 13:49:15 +0530

    Update run_edge.bat

--- run_edge.bat
+++ run_edge.bat
@@ -1,1 +1,1 @@
-"C:\Users\hp\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe" -v -s --disable-warnings -m "group2" --browser edge --html=HTMLReports/xyzCbat.html -n=2 --alluredir="AllureReports"
+"C:\Users\hp\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe" -v -s --disable-warnings -m "group2" --browser chrome --html=HTMLReports/xyzCbat.html -n=2 --alluredir="AllureReports"
