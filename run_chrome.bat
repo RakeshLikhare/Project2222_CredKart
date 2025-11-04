8a547707865848d720e227ab1996a4d04d95385c
Rakeshji <rakeshlikhare@gmail.com>
2025-11-04 14:00:15 +0530

    change commit of code in local for browser

---
+++ run_chrome.bat
@@ -0,0 +1,1 @@
+"C:\Users\hp\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe" -v -s --disable-warnings -m "group2" --browser chrome --html=HTMLReports/xyzAbat.html -n=2 --alluredir="AllureReports"
--- run_edge.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\hp\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe" -v -s --disable-warnings -m "group2" --browser chrome --html=HTMLReports/xyzCbat.html -n=2 --alluredir="AllureReports"
